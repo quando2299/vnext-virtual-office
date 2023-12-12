161d10a6910d143905d8d4324efecb24706d2187
Do Hieu Quan 2 <quandh2@vnext.vn>
2023-12-12 11:12:13 +0700

    Add Component, hook
    - Update completely component
    - Add hook, util function
    - Update package.json

--- apps/virtual-office/src/scenes/Game.ts
+++ apps/virtual-office/src/scenes/Game.ts
@@ -1,6 +1,10 @@
 import Network from '../services/Network';
 
 export default class Game extends Phaser.Scene {
+  myPlayer: any;
+  registerKeys() {
+    throw new Error('Method not implemented.');
+  }
   network!: Network;
   disableKeys() {
     this.input.keyboard!.enabled = false;
